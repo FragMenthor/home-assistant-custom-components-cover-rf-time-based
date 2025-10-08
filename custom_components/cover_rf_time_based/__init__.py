87e0a4d44c07b5e9c146404de3ec4c74b99f7f0e
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2025-10-08 12:04:49 +0100

    Update __init__.py

--- custom_components/cover_rf_time_based/__init__.py
+++ custom_components/cover_rf_time_based/__init__.py
@@ -1,41 +1,12 @@
 """Cover RF Time Based integration."""
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import service as ha_service
-from homeassistant.components.cover import DOMAIN as COVER_DOMAIN
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+from .const import DOMAIN, SERVICE_SET_KNOWN_POSITION, SERVICE_SET_KNOWN_ACTION
+import logging
 
-from .cover import SERVICE_SET_KNOWN_POSITION, SERVICE_SET_KNOWN_ACTION
-
-DOMAIN = "cover_rf_time_based"
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the cover_rf_time_based integration (config via YAML)."""
-
-    # Registrar os serviços de entidade de forma global
-    ha_service.async_register_platform_entity_service(
-        hass,
-        DOMAIN,
-        SERVICE_SET_KNOWN_POSITION,
-        entity_domain=COVER_DOMAIN,
-        schema={
-            vol.Required("position"): cv.positive_int,
-            vol.Optional("confident", default=False): cv.boolean,
-            vol.Optional("position_type", default="target"): cv.string,
-        },
-        func="set_known_position",
-    )
-
-    ha_service.async_register_platform_entity_service(
-        hass,
-        DOMAIN,
-        SERVICE_SET_KNOWN_ACTION,
-        entity_domain=COVER_DOMAIN,
-        schema={
-            vol.Required("action"): vol.In(["open", "close", "stop"]),
-        },
-        func="set_known_action",
-    )
-
+    """Set up cover_rf_time_based (config via YAML)."""
+    _LOGGER.debug("Integração cover_rf_time_based inicializada.")
     return True
