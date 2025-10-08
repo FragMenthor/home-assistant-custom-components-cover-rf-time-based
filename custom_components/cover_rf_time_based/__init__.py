a8e67cf3abde36ac5822e8ebaaad304af8bfe4eb
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2025-10-08 14:16:31 +0100

    Update __init__.py

--- custom_components/cover_rf_time_based/__init__.py
+++ custom_components/cover_rf_time_based/__init__.py
@@ -1,12 +1,11 @@
 """Cover RF Time Based integration."""
 
-from homeassistant.core import HomeAssistant
-from .const import DOMAIN, SERVICE_SET_KNOWN_POSITION, SERVICE_SET_KNOWN_ACTION
 import logging
+from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up cover_rf_time_based (config via YAML)."""
-    _LOGGER.debug("Integração cover_rf_time_based inicializada.")
+    """Inicializa a integração cover_rf_time_based."""
+    _LOGGER.debug("Integração cover_rf_time_based inicializada")
     return True
