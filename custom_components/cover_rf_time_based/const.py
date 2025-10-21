78e7ad571793dabfaa4acd9f9d006f51fe921363
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2025-10-21 09:53:36 +0100

    Update const.py

--- custom_components/cover_rf_time_based/const.py
+++ custom_components/cover_rf_time_based/const.py
@@ -1,25 +1,13 @@
-"""Constantes da integração cover_rf_time_based."""
+"""Constantes para a integração cover_time_based."""
 
-DOMAIN = "cover_rf_time_based"
+DOMAIN = "cover_time_based"
 
-CONF_DEVICES = "devices"
-CONF_ALIASES = "aliases"
-CONF_TRAVELLING_TIME_DOWN = "travelling_time_down"
+CONF_NAME = "name"
 CONF_TRAVELLING_TIME_UP = "travelling_time_up"
+CONF_TRAVELLING_TIME_DOWN = "travelling_time_down"
+CONF_OPEN_SCRIPT = "open_script_entity_id"
+CONF_CLOSE_SCRIPT = "close_script_entity_id"
+CONF_STOP_SCRIPT = "stop_script_entity_id"
 CONF_SEND_STOP_AT_ENDS = "send_stop_at_ends"
+CONF_ALIASES = "aliases"
 CONF_ALWAYS_CONFIDENT = "always_confident"
-
-DEFAULT_TRAVEL_TIME = 25
-DEFAULT_SEND_STOP_AT_ENDS = False
-DEFAULT_ALWAYS_CONFIDENT = False
-
-SERVICE_SET_KNOWN_POSITION = "set_known_position"
-SERVICE_SET_KNOWN_ACTION = "set_known_action"
-
-ATTR_POSITION = "position"
-ATTR_CONFIDENT = "confident"
-ATTR_POSITION_TYPE = "position_type"
-ATTR_ACTION = "action"
-
-ATTR_POSITION_TYPE_TARGET = "target"
-ATTR_POSITION_TYPE_CURRENT = "current"
