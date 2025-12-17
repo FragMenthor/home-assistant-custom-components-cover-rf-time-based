0191eb0b2aac290b5cadcd0f2df44b33b44b85a2
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2025-12-17 19:33:49 +0000

    Refactor config flow by removing options handling

--- custom_components/cover_rf_time_based/config_flow.py
+++ custom_components/cover_rf_time_based/config_flow.py
@@ -26,6 +26,7 @@ class CoverTimeBasedSyncFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Primeiro passo do fluxo."""
         if user_input is not None:
             return self.async_create_entry(
                 title=user_input[CONF_NAME],
@@ -115,40 +116,6 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                     CONF_ALIASES,
                     default=options.get(CONF_ALIASES, data.get(CONF_ALIASES, "")),
                 ): str,
-            }
-        )
-
-        return self.async_show_form(step_id="init", data_schema=schema)                vol.Optional(CONF_ALIASES, default=""): str,
-            }
-        )
-
-        return self.async_show_form(step_id="user", data_schema=schema)
-
-    @staticmethod
-    def async_get_options_flow(entry):
-        return OptionsFlowHandler(entry)
-
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, entry):
-        self.entry = entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        data = self.entry.data
-        options = self.entry.options
-
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_TRAVELLING_TIME_UP, default=options.get(CONF_TRAVELLING_TIME_UP, data.get(CONF_TRAVELLING_TIME_UP))): int,
-                vol.Required(CONF_TRAVELLING_TIME_DOWN, default=options.get(CONF_TRAVELLING_TIME_DOWN, data.get(CONF_TRAVELLING_TIME_DOWN))): int,
-                vol.Optional(CONF_OPEN_SCRIPT, default=options.get(CONF_OPEN_SCRIPT, data.get(CONF_OPEN_SCRIPT))): str,
-                vol.Optional(CONF_CLOSE_SCRIPT, default=options.get(CONF_CLOSE_SCRIPT, data.get(CONF_CLOSE_SCRIPT))): str,
-                vol.Optional(CONF_STOP_SCRIPT, default=options.get(CONF_STOP_SCRIPT, data.get(CONF_STOP_SCRIPT))): str,
-                vol.Optional(CONF_SEND_STOP_AT_ENDS, default=options.get(CONF_SEND_STOP_AT_ENDS, data.get(CONF_SEND_STOP_AT_ENDS, False))): bool,
-                vol.Optional(CONF_ALIASES, default=options.get(CONF_ALIASES, data.get(CONF_ALIASES, ""))): str,
             }
         )
 
