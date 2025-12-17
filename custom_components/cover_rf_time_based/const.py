eee914e256a03a6cff379088d35467d4c6c7d772
FragMenthor <74264069+FragMenthor@users.noreply.github.com>
2025-12-17 19:11:37 +0000

    Update constants for Cover Time Based Sync integration

--- custom_components/cover_rf_time_based/const.py
+++ custom_components/cover_rf_time_based/const.py
@@ -1,6 +1,6 @@
-"""Constantes para a integração cover_time_based."""
+"""Constantes para a integração Cover Time Based Sync."""
 
-DOMAIN = "cover_time_based"
+DOMAIN = "cover_time_based_sync"
 
 CONF_NAME = "name"
 CONF_TRAVELLING_TIME_UP = "travelling_time_up"
@@ -11,3 +11,13 @@ CONF_STOP_SCRIPT = "stop_script_entity_id"
 CONF_SEND_STOP_AT_ENDS = "send_stop_at_ends"
 CONF_ALIASES = "aliases"
 CONF_ALWAYS_CONFIDENT = "always_confident"
+
+SERVICE_SET_KNOWN_POSITION = "set_known_position"
+SERVICE_SET_KNOWN_ACTION = "set_known_action"
+
+ATTR_POSITION = "position"
+ATTR_CONFIDENT = "confident"
+ATTR_POSITION_TYPE = "position_type"
+ATTR_POSITION_TYPE_TARGET = "target"
+ATTR_POSITION_TYPE_CURRENT = "current"
+ATTR_ACTION = "action"
